dc7ca9643c67ddb8488987e8e8de534a40bee80d
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-15 14:24:22 +0100

    had added name input

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -2,9 +2,15 @@ require 'sinatra/base'
 
 class BattleshipWeb < Sinatra::Base
   get '/' do
-    'Hello BattleshipWeb!'
+    erb :index
   end
 
+  get '/name' do
+    erb :name_form
+  end
+
+  set :views, proc { File.join(root, '..', 'views')}
   # start the server if ruby file executed directly
   run! if app_file == $0
+
 end
