71eed73bb595ec01a87bf157a8feb43e137b8702
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-16 10:06:09 +0100

    Fixed error

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,20 +1,22 @@
 require 'sinatra/base'
+require_relative 'board'
+require_relative 'ship'
+require_relative 'cell'
+require_relative 'water'
 
 class BattleshipWeb < Sinatra::Base
+
+  set :views, proc { File.join(root, '..', 'views')}
+
   get '/' do
     erb :index
   end
 
   get '/name' do
   	@name = params[:name]
-    erb :name_form
+    erb :new_game
   end
 
-  get '/board' do
-  	erb :board
-  end
-
-  set :views, proc { File.join(root, '..', 'views')}
   # start the server if ruby file executed directly
   run! if app_file == $0
 
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -2,18 +2,26 @@ require 'spec_helper'
 
 feature 'Starting a new game' do
 
-  scenario 'I am asked to enter my name' do
+  scenario 'Asked to enter my name' do
     visit '/'
     click_link 'New Game'
     expect(page).to have_content "What's your name?"
   end
 
-  scenario 'A board is rendered in the browser when a name is entered' do
+  scenario 'Recieves name' do
   	visit '/'
-  	click_link 'New Game'
+    click_link 'New Game'
   	fill_in('name', :with => 'Harry')
   	click_button 'Submit'
-  	expect(page).to have_content 'Im a board'
+  	expect(page).to have_content "Harry's Game!"
   end
+
+  # scenario 'Does not receive name' do
+  #   visit '/'
+  #   click_link 'New Game'
+  #   fill_in 'name', with: ''
+  #   click_button 'Submit'
+  #   expect(page).to have_content 'Please enter your name'
+  # end
 
 end
